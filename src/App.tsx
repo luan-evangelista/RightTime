f6162353383b58021acc62cf3810c6ce1d6ea111
Luan <luan.evieira43@gmail.com>
2024-01-24 19:46:46 -0300

    Add Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
+import AuthContext from './contexts/auth';
+
 import Routes from './routes';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <Routes />
+      <AuthContext.Provider value={{signed: true}}>
+        <Routes />
+      </AuthContext.Provider>
     </NavigationContainer>
   );
 };
